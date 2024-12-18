c92da76423366632671f8554e59867ebe3aea2ef
Hudson <hudsoncdiniz@gmail.com>
2024-12-18 17:35:54 -0300

    Update LoginPage

--- coordina-app/src/components/LoginPage.jsx
+++ coordina-app/src/components/LoginPage.jsx
@@ -1,12 +1,11 @@
-import {FaUser, FaLock} from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
 import logo from "../assets/img/logo.png";
-
 
 const LoginPage = () => {
   return (
-    <div className="flex items-center w-screen h-screen bg-slate-600 justify-center p-6">
-      <div className="w-80 h-auto bg-white p-6 text-center shadow-md rounded-lg">
-        <img src={logo} alt="logo" className="w-24 h-auto mx-auto mb-4"/>
+    <div className="flex items-center w-screen h-screen bg-[#F19E38] justify-center p-6">
+      <div className="w-80 h-auto bg-white p-6 text-center shadow-xl rounded-xl">
+        <img src={logo} alt="logo" className="w-24 h-auto mx-auto mb-4" />
         <form action="#" className="flex flex-col gap-3">
           <div className="flex flex-col gap-1 relative">
             <label htmlFor="user" className="text-left">
@@ -30,9 +29,9 @@ const LoginPage = () => {
               placeholder="Digite sua senha"
               className="border border-gray-300 rounded-lg pl-7 py-2 hover:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
             />
-            <FaLock className="absolute top-10 left-2"/>
+            <FaLock className="absolute top-10 left-2" />
           </div>
-          <button className="flex w-[100%] justify-center rounded-full px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 mt-3">
+          <button className="flex w-[100%] justify-center rounded-full px-4 py-2 bg-blue-500 text-white hover:bg-[#ed9934] mt-3 font-bold bg-[#F19E38]">
             Entrar
           </button>
         </form>
